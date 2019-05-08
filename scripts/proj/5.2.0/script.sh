f19543f1fedfb2ba2c26e2c14ae5c6ab1d5af051
Raul Marin <git@rmr.ninja>
2019-05-08 13:02:35 +0200

    Proj 5.2.0: Patch unit test to add pthread

--- scripts/proj/5.2.0/script.sh
+++ scripts/proj/5.2.0/script.sh
@@ -22,6 +22,42 @@ function mason_compile {
     cd nad
     unzip -o ../proj-datumgrid-${GRID_VERSION}.zip
     cd ../
+
+    echo 'AUTOMAKE_OPTIONS = subdir-objects
+
+EXTRA_DIST = CMakeLists.txt
+
+noinst_HEADERS = gtest_include.h
+
+AM_CPPFLAGS = -I$(top_srcdir)/src -I$(top_srcdir)/test -I$(top_srcdir)/test/googletest/include
+AM_CXXFLAGS = @CXX_WFLAGS@ @NO_ZERO_AS_NULL_POINTER_CONSTANT_FLAG@
+
+noinst_PROGRAMS = basic_test
+noinst_PROGRAMS += pj_phi2_test
+noinst_PROGRAMS += proj_errno_string_test
+
+basic_test_SOURCES = basic_test.cpp main.cpp
+basic_test_LDADD = ../../src/libproj.la ../../test/googletest/libgtest.la -lpthread
+
+basic_test-check: basic_test
+	./basic_test
+
+pj_phi2_test_SOURCES = pj_phi2_test.cpp main.cpp
+pj_phi2_test_LDADD = ../../src/libproj.la ../../test/googletest/libgtest.la -lpthread
+
+pj_phi2_test-check: pj_phi2_test
+	./pj_phi2_test
+
+proj_errno_string_test_SOURCES = proj_errno_string_test.cpp main.cpp
+proj_errno_string_test_LDADD= ../../src/libproj.la ../../test/googletest/libgtest.la -lpthread
+
+proj_errno_string_test-check: proj_errno_string_test
+	./proj_errno_string_test
+
+check-local: basic_test-check
+check-local: pj_phi2_test-check proj_errno_string_test-check
+' > test/unit/Makefile.am
+
     # note CFLAGS overrides defaults (-g -O2) so we need to add optimization flags back
     export CFLAGS="${CFLAGS} -O3 -DNDEBUG"
     ./configure --prefix=${MASON_PREFIX} \
